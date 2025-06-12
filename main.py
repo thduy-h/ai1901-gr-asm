4f2b9cec8caa2a8e5f9a6bcc90e38d86743ae1fe
Thanh Duy - Lead <hthanhduyy@gmail.com>
2025-06-12 12:40:07 +0700

    them def cho Bảo, MinhLe

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from Member.hello_duy import hello_d
+from Member.hello_minhle import hello_minhle
+from Member.hello_quocbao import hello_quocbao
 
 def main():
     hello_d()
-
+    hello_minhle()
+    hello_quocbao()
 if __name__ == "__main__":
     main()
     
