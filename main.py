a9f9c9f83b3e767f2c753e1e6f9413d37bc47e08
Thanh Duy - Lead <hthanhduyy@gmail.com>
2025-06-12 12:52:53 +0700

    sửa def cho LeMinh

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from Member.hello_duy import hello_d
 from Member.hello_minhle import hello_minhle
 from Member.hello_quocbao import hello_quocbao
-from Member.hello_minh import hello_minh
+from Member.hello_minh import hello_m
 
 def main():
     hello_d()
     hello_minhle()
     hello_quocbao()
-    hello_minh()
+    hello_m()
 if __name__ == "__main__":
     main()
     
