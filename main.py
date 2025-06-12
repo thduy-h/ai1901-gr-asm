07541e4083eae77a73bba470ba9aaa767426d2b4
Thanh Duy - Lead <hthanhduyy@gmail.com>
2025-06-12 12:49:34 +0700

    them def cho LeMinh

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from Member.hello_duy import hello_d
 from Member.hello_minhle import hello_minhle
 from Member.hello_quocbao import hello_quocbao
+from Member.hello_minh import hello_minh
 
 def main():
     hello_d()
     hello_minhle()
     hello_quocbao()
+    hello_minh()
 if __name__ == "__main__":
     main()
     
